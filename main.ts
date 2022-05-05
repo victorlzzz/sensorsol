f6526c29aaeeddf03ac234da7adff7ad8a6f9b68
victorlzzz <105002724+victorlzzz@users.noreply.github.com>
2022-05-05 17:48:50 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 enum RadioMessage {
     message1 = 49434,
     sol = 8468,
-    solO = 62579
+    solO = 62579,
+    nube = 62918,
+    nubeO = 47874,
+    solnube = 33757,
+    solnubeO = 1891
 }
 basic.forever(function () {
     if (envirobit.getPressure() >= 0) {
@@ -12,27 +16,21 @@ basic.forever(function () {
 })
 basic.forever(function () {
     if (envirobit.getPressure() >= 0) {
-        radio.sendMessage(RadioMessage.sol)
+        radio.sendMessage(RadioMessage.nube)
     } else {
-        radio.sendMessage(RadioMessage.solO)
+        radio.sendMessage(RadioMessage.nubeO)
     }
 })
 basic.forever(function () {
     if (envirobit.getPressure() >= 0) {
-        radio.sendMessage(RadioMessage.sol)
+        radio.sendMessage(RadioMessage.solnube)
     } else {
-        radio.sendMessage(RadioMessage.solO)
+        radio.sendMessage(RadioMessage.solnubeO)
     }
 })
 basic.forever(function () {
     basic.showNumber(envirobit.getPressure())
     basic.pause(5000)
     basic.showNumber(envirobit.getTemperature())
-})
-basic.forever(function () {
-    if (envirobit.getPressure() >= 0) {
-        radio.sendMessage(RadioMessage.sol)
-    } else {
-        radio.sendMessage(RadioMessage.solO)
-    }
+    basic.pause(5000)
 })
